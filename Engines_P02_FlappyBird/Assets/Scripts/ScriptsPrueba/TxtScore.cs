7ab228e02d2989dafb7d1db434c4327cb7b82d11
Feer-Plata <fernando.platacbc@gmail.com>
2022-11-07 12:23:37 -0600

    main - panel principal
    Creacion de panel principal y reajuste de spawn

--- Engines_P02_FlappyBird/Assets/Scripts/ScriptsPrueba/TxtScore.cs
+++ Engines_P02_FlappyBird/Assets/Scripts/ScriptsPrueba/TxtScore.cs
@@ -2,24 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+using TMPro;
 public class TxtScore : MonoBehaviour
 {
     //Atributos
-
-    public Text scoreTxt;
-    public Text highScoreTxt;
+    public static int puntaje = 0;
+    void Start()
+    {
+        puntaje = 0;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        scoreTxt.text = "Score: " + Score.score;
-        highScoreTxt.text = "HighScore: " + Score.highScore;
+        GetComponent<TextMeshProUGUI>().text = puntaje.ToString();
     }
 
+    //public Text scoreTxt;
+    //public Text highScoreTxt;
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    scoreTxt.text = "Score: " + Score.score;
+    //    highScoreTxt.text = "HighScore: " + Score.highScore;
+    //}
+
 }
-public class Score : MonoBehaviour
-{
-    public static int score = 0;                    //Inicializa el score en cero.
-    public static int highScore = 0;                //Inicializa el highScore en cero.
-}
+//public class Score : MonoBehaviour
+//{
+//    public static int score = 0;                    //Inicializa el score en cero.
+//    public static int highScore = 0;                //Inicializa el highScore en cero.
+//}
