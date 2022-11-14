2fd73ea2494e573bc41c101ca2fe26e56dcd9eaf
Feer-Plata <fernando.platacbc@gmail.com>
2022-11-14 12:35:54 -0600

    main - ajustes de configuracioens

--- Engines_P02_FlappyBird/Assets/Scripts/CambioEscenas.cs
+++ Engines_P02_FlappyBird/Assets/Scripts/CambioEscenas.cs
@@ -2,12 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
 public class CambioEscenas : MonoBehaviour
 {
     public void EmpezarJuego()
     {
         SceneManager.LoadScene("JuegoFinal");
     }
-
 }
